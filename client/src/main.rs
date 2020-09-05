5a82576547bbac9f0f2629ba9333b4f0304835df
CeciliaZ030 <CeciliaZ030@users.noreply.github.com>
2020-09-05 19:38:20 -0400

    sc with param

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,21 +4,22 @@ use zmq::SNDMORE;
 fn main() {
 
     let context = zmq::Context::new();
-    let dealer = context.socket(zmq::DEALER).unwrap();
+    let client = context.socket(zmq::DEALER).unwrap();
 
     let identity = "Alice";
     println!("Alice's name as_bytes {:?}", identity.as_bytes());
-    dealer.set_identity(identity.as_bytes());
+    client.set_identity(identity.as_bytes());
 
-    assert!(dealer.connect("tcp://localhost:6000").is_ok());
+    assert!(client.connect("tcp://localhost:6000").is_ok());
 
-    loop {
+    client.send("Hello", SNDMORE).unwrap();
+    client.send("blah", 0).unwrap();
 
-        dealer.send("fetch", SNDMORE).unwrap();
-        dealer.send("645", 0).unwrap();
+    let Gx = client.recv_string(0).unwrap().unwrap();
+    let Gy = client.recv_string(0).unwrap().unwrap();
+    let a = client.recv_string(0).unwrap().unwrap();
+    let b = client.recv_string(0).unwrap().unwrap();
+    let P = client.recv_string(0).unwrap().unwrap();
 
-        let chunk = dealer.recv_string(0).unwrap().unwrap();
-        println!("{:?} chunks received", chunk);
-    }
-
+    println!("{}, {}, {}, {}, {}", Gx, Gy, a, b, P);
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,34 +2,42 @@
 //! Hello World server in Rust
 //! Binds REP socket to tcp://*:5555
 //! Expects "Hello" from client, replies with "World"
-use std::thread;
 use zmq::SNDMORE;
 
 
 fn main() {
-    let context = zmq::Context::new();
-    let router = context.socket(zmq::ROUTER).unwrap();
 
-    assert!(router.bind("tcp://*:6000").is_ok());
+    let context = zmq::Context::new();
+    let server = context.socket(zmq::ROUTER).unwrap();
+
+    assert!(server.bind("tcp://*:6000").is_ok());
+
+    let Gx = "48439561293906451759052585252797914202762949526041747995844080717082404635286";
+    let Gy = "36134250956749795798585127919587881956611106672985015071877198253568414405109";
+    let a = "-3";
+    let b = "41058363725152142129326129780047268409114441015993725554835256314039467401291";
+    let P = "115792089210356248762697446949407573530086143415290314195533631308867097853951";
+
 
     loop {
         // First frame in each message is the sender identity
-        let identity = router.recv_bytes(0).unwrap();
+        let identity = server.recv_bytes(0).unwrap();
         if identity.is_empty() {
             break; //  Shutting down, quit
         }
+        println!("{:?} joining", String::from_utf8((&identity).clone()).unwrap());
 
-        // Second frame is "fetch" command
-        let command = router.recv_string(0).unwrap().unwrap();
-        assert!(command == "fetch");
-
-        // Third frame is chunk offset in file
-        let offset = router.recv_string(0).unwrap().unwrap();
-        let offset = offset.parse::<u64>().unwrap();
+        let msg = server.recv_string(0).unwrap().unwrap();
+        let more_words = server.recv_string(0).unwrap().unwrap();
 
 
         // Send resulting chunk to client
-        router.send(&identity, SNDMORE).unwrap();
-        router.send("World", 0).unwrap();
+        server.send(&identity, SNDMORE).unwrap();
+        server.send(&Gx, SNDMORE).unwrap();
+        server.send(&Gy, SNDMORE).unwrap();
+        server.send(&a, SNDMORE).unwrap();
+        server.send(&b, SNDMORE).unwrap();
+        server.send(&P, 0).unwrap();
+        println!("sent");
     }
 }
