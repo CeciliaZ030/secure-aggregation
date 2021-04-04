2c0dbc393b3a5257ab0f18759fed2b58d9ed467d
CeciliaZ030 <45245961+CeciliaZ030@users.noreply.github.com>
2021-04-04 18:41:00 +0400

    Update sockets.rs

--- client/src/sockets.rs
+++ client/src/sockets.rs
@@ -34,42 +34,70 @@ pub fn recv(socket: &Socket) -> RecvType {
 	}
 }
 
-// pub fn recv_broadcast(socket: &Socket, topic: &str) -> Result<RecvType, ()> {
-// 	let mut data;
-// 	match socket.recv_multipart(0) {
-// 		Ok(msg) => {
-// 				data = msg;
-// 			},
-// 		Err(_) => panic!("Failed to recieve braoadcast."),
-// 	}
-// 	let removed = data.remove(0);
-// 	if (removed != topic.as_bytes()) {
-// 		println!("{:?}, {:?}", str::from_utf8(&removed), topic);
-// 		return Err(());
-// 	}
-// 	if data.len() == 1 {
-// 		match std::str::from_utf8(&data[0]) {
-// 			Ok(s) => return Ok(RecvType::string(s.to_string())),
-// 			Err(_) => return Ok(RecvType::bytes(data.pop().unwrap())),
-// 		};
-// 	} else {
-// 		return Ok(RecvType::matrix(data));
-// 	}
-// }
+pub fn recv_broadcast(socket: &Socket, topic: &str) -> Result<RecvType, ()> {
+	let mut data = match socket.recv_multipart(0) {
+		Ok(msg) => msg,
+		Err(_) => panic!("Failed to recieve braoadcast."),
+	};
+	if data.len() == 0 {
+		return Ok(RecvType::bytes(Vec::new()));
+	}
+	let removed = data.remove(0);
+	if (removed != topic.as_bytes()) {
+		println!("recv_broadcast err {:?}, {:?}", str::from_utf8(&removed), topic);
+		return Err(());
+	}
+	let mut stringRes = String::new();
+	let mut isString = true;
+	for d in &data {
+		match str::from_utf8(d) {
+			Ok(s) => stringRes += s,
+			Err(_) => {
+				isString = false;
+				break
+			},
+		};
+	}
+	if !isString {
+		if data.len() == 1 {
+			return Ok(RecvType::bytes(data.pop().unwrap()));
+		} else {
+			return Ok(RecvType::matrix(data));
+		}
+	} else {
+		return Ok(RecvType::string(stringRes));
+	}
+}
 
-pub fn recv_broadcast1(socket: &Socket) -> (Vec<u8>, RecvType) {
+pub fn consume_broadcast(socket: &Socket) -> (Vec<u8>, RecvType) {
 	let mut data = match socket.recv_multipart(0) {
 		Ok(msg) => msg,
 		Err(_) => panic!("Failed to recieve braoadcast."),
 	};
 	let topic = data.remove(0);
-	if data.len() == 1 {
-		match std::str::from_utf8(&data[0]) {
-			Ok(s) => return (topic, RecvType::string(s.to_string())),
-			Err(_) => return (topic, RecvType::bytes(data.pop().unwrap())),
+	if str::from_utf8(&topic).unwrap() == "AG" {println!("{:?}", data);}
+	if data.len() == 0 {
+		return (topic, RecvType::bytes(Vec::new()));
+	}
+	let mut stringRes = String::new();
+	let mut isString = true;
+	for d in &data {
+		match str::from_utf8(d) {
+			Ok(s) => stringRes += s,
+			Err(_) => {
+				isString = false;
+				break
+			},
 		};
+	}
+	if !isString {
+		if data.len() == 1 {
+			return (topic, RecvType::bytes(data.pop().unwrap()));
+		} else {
+			return (topic, RecvType::matrix(data));
+		}
 	} else {
-		return (topic, RecvType::matrix(data));
+		return (topic, RecvType::string(stringRes));
 	}
 }
 
