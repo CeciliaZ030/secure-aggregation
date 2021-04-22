541bf8b874e28975c01deee0dbb02b136e72a301
Zhang <yzh223@rett-imac-01.ur.rochester.edu>
2021-04-22 02:15:50 -0400

    pss

--- pss/src/main.rs
+++ pss/src/main.rs
@@ -15,8 +15,8 @@ fn main() {
         512, 729, 51200, 512, 700);
 
     let mut rng = thread_rng();
-    let mut secrets = vec![0u64; 5120];
-    for i in 0..5120 {
+    let mut secrets = vec![0u64; 5120*2];
+    for i in 0..5120*2 {
         secrets[i] = rng.gen_range(0, u64::MAX);
     }
     let shares = pss.share(&secrets);
