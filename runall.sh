89836b231f60d8f84c831910d1143d8ea72da9fc
Ubuntu <ubuntu@ip-172-31-19-98.ec2.internal>
2021-04-23 06:02:01 +0000

    more new data

--- runall.sh
+++ runall.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 cd server
-cargo run $1 $2 $3 $4 $5 $6 $7&
+cargo run $6 $7 $1 $2 $3 $4 $5 &
 
 cd ..
 cd client
-for (( counter=0; counter< $1; counter++ ))
+for (( counter=0; counter< $6; counter++ ))
 do
-sleep 0.01 && (target/debug/client "Client $counter" $2 & echo "Client $counter")
+sleep 0.01 && (target/debug/client "Client $counter" $7 & echo "Client $counter")
 done
 
 #END
