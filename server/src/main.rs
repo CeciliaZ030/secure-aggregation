2802ab1943c11bf6a8304a8aa3863da3bf7b2930
Ubuntu <ubuntu@ip-172-31-88-29.ec2.internal>
2021-05-05 03:17:47 +0000

    ec-result

--- server/src/main.rs
+++ server/src/main.rs
@@ -104,7 +104,7 @@ fn main() {
         infoms state thread once successfully process 1 msg.
     */
 	let mut workerThreadPool = Vec::new();
-	for i in 0..11 {
+	for i in 0..32 {
 		let worker = Worker::new(
             &format!("Worker{}", i.to_string()),
             context.clone(), 
